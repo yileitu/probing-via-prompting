8ad273d9a5e7c40e19df0f9dbe280abff2dac3d8
yileitu <yileitu@student.ethz.ch>
2022-11-12 21:33:43 +0100

    wandb

---
+++ run_pp_ner.sh
@@ -0,0 +1,20 @@
+export WANDB_PROJECT=PvP-NER
+export TASK_NAME=ner
+
+python3 run_pp.py \
+  --seed 21946520 \
+  --do_train \
+  --do_eval \
+  --per_device_train_batch_size 4 \
+  --per_device_eval_batch_size 4 \
+  --gpt2_name_or_path gpt2 \
+  --data_dir ontonotes/pp/ \
+  --task $TASK_NAME \
+  --output_dir outputs/pp/$TASK_NAME/ \
+  --overwrite_output_dir \
+  --use_fast_tokenizer False \
+  --cache_dir cache/ \
+  --save_strategy no \
+  --num_train_epochs 3.0 \
+  --learning_rate 5e-5 \
+  --prefix_len 200
